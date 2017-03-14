d03b595440cf2dd20e7400d227f6f3d8b4934aa5
Shengwen Cheng <shengwen1997.tw@gmail.com>
2017-03-14 16:11:11 +0800

    Finish the exhaust searching feature mapping, but not workable yet

--- static_optical_flow/main.cpp
+++ static_optical_flow/main.cpp
@@ -11,10 +11,14 @@
 using namespace std;
 using namespace cv;
 
-#define SAMPLE_RATE 5 //down sampling factor = 1/5
-#define FRAME_SIZE (640 / SAMPLE_RATE) //640x640 -> 128x128
-#define TEMPLATE_SIZE 8 //8x8
-#define SEARCH_AREA 24  //24x24
+#define SAMPLE_RATE 5                      //down sampling factor = 1/5
+
+#define FRAME_SIZE (640 / SAMPLE_RATE)     //640x640 -> 128x128
+#define TEMPLATE_SIZE 8                    //8x8
+#define SEARCH_AREA 24                     //24x24
+#define SUBAREA_SEMI_LEN (SEARCH_AREA / 2) //12
+
+#define TEMPLATE_NUMBER (FRAME_SIZE - TEMPLATE_SIZE + 1) //number 8x8 template split from the frame
 
 #define MAD_THRESHOLD 255 //only accept mad when the value is lower than this threshold
 
@@ -24,7 +28,7 @@ cv::Mat mat_frame2_img;
 uint8_t frame1_image[FRAME_SIZE][FRAME_SIZE] = {0}; //previous frame
 uint8_t frame2_image[FRAME_SIZE][FRAME_SIZE] = {0}; //next image
 
-flow_t flow_info[FRAME_SIZE][FRAME_SIZE];
+flow_t flow_info[TEMPLATE_NUMBER][TEMPLATE_NUMBER];
 
 void read_image()
 {
@@ -66,10 +70,10 @@ uint8_t mean_abs_diff(uint8_t *search_area, uint8_t *search_template)
 	uint32_t mad = 0;
 
 	int i, j;
-	for(i = 0; i < FRAME_SIZE; i++) {
-		for(j = 0; j < FRAME_SIZE; j++) {
+	for(i = 0; i < TEMPLATE_SIZE; i++) {
+		for(j = 0; j < TEMPLATE_SIZE; j++) {
 			mad += abs(
-				search_template[i * TEMPLATE_SIZE + j] -
+				search_template[i * FRAME_SIZE + j] -
 				search_area[i * FRAME_SIZE + j]
 			);
 		}
@@ -93,7 +97,41 @@ bool calculate_subarea_mad(uint8_t *search_area, uint8_t *search_template,
 		for(j = 0; j < search_size; j++) {
 			mad = mean_abs_diff(
 				&search_area[i * FRAME_SIZE + j],
-				&search_template[i * TEMPLATE_SIZE + j]
+				&search_template[i * FRAME_SIZE + j]
+			);
+
+			if(mad < min_mad) {
+				_match_x = i;
+				_match_y = j;
+				min_mad = mad;
+			}
+		}
+	}
+
+	if(min_mad > MAD_THRESHOLD) {
+		return false;
+	}
+
+	/* position on the subarea, not the whole frame! */
+	*match_x = _match_x;
+	*match_y = _match_y;
+
+	return true;
+}
+
+/* calculate 8x8 template's mad value on the whole frame */
+bool calculate_mad_full_frame(uint8_t *frame, uint8_t *search_template,
+	int *match_x, int *match_y)
+{
+	int _match_x = 0, _match_y = 0;
+	uint8_t min_mad = 255, mad; //initial rest
+
+	int i, j;
+	for(i = 0; i < TEMPLATE_NUMBER; i++) {
+		for(j = 0; j < TEMPLATE_NUMBER; j++) {
+			mad = mean_abs_diff(
+				&frame[i * FRAME_SIZE + j],
+				&search_template[i * FRAME_SIZE + j]
 			);
 
 			if(mad < min_mad) {
@@ -116,21 +154,58 @@ bool calculate_subarea_mad(uint8_t *search_area, uint8_t *search_template,
 }
 
 /* match all shift feature points on two frames */
-void match_feature_points(uint8_t last_frame, uint8_t curr_frame)
+void match_feature_points(uint8_t *last_frame, uint8_t *curr_frame)
 {
-	int running_size;
+	bool match;
+	int match_x = 0, match_y = 0;
 
 	int i, j;
-	for(i = 0; i < running_size; i++) {
-		for(j = 0; j < running_size; j++) {
-			
+	for(i = 0; i < TEMPLATE_NUMBER; i++) {
+		for(j = 0; j < TEMPLATE_NUMBER; j++) {
+			match = calculate_mad_full_frame(
+				&last_frame[i * FRAME_SIZE + j],
+				&curr_frame[i * FRAME_SIZE + j],
+				&match_x, &match_y
+			);
+
+			if(match == true) {
+				flow_info[i][j].match_point.x = match_x;
+				flow_info[i][j].match_point.y = match_y;
+				flow_info[i][j].no_match_point = false;
+				flow_info[i][j].match_dist = sqrt(
+					(match_x - i * SAMPLE_RATE) *
+					(match_x - i * SAMPLE_RATE) +
+					(match_y - j * SAMPLE_RATE) *
+					(match_y - j * SAMPLE_RATE)
+				);
+			} else {
+				flow_info[i][j].no_match_point = true;
+			}
 		}
 	}
 }
 
 void match_point_visualize(cv::Mat& frame)
 {
-	//cv::circle(frame, Point(x, y), 1, Scalar(0, 255, 0), 2, CV_AA, 0);
+	int x;
+	int y;
+
+	for(int i = 0; i < TEMPLATE_NUMBER; i++) {
+		for(int j = 0; j < TEMPLATE_NUMBER; j++) {
+			if(flow_info[i][j].no_match_point == true) {
+				continue;
+			}
+
+			if(flow_info[i][j].match_dist < 20) {
+				continue;	
+			}
+
+			x = flow_info[i][j].match_point.x * SAMPLE_RATE;
+			y = flow_info[i][j].match_point.y * SAMPLE_RATE;
+
+			cv::circle(frame, Point(x, y), 1, Scalar(0, 255, 0), 2, CV_AA, 0);
+		}
+	}
 }
 
 void flow_visualize(cv::Mat& frame)
@@ -140,6 +215,8 @@ void flow_visualize(cv::Mat& frame)
 int main()
 {
 	read_image();
+
+	match_feature_points(&frame1_image[0][0], &frame2_image[0][0]);
 
 	//flow_visualize(mat_frame1_img);
 	//match_point_visualize(mat_frame1_img);
